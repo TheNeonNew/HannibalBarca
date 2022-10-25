ca4497d96b87a58e96e3f35b4d608224db59528a
Vlad Skarnovich <skarnovichvm@mer.ci.nsu.ru>
2022-10-25 16:13:22 +0700

    Add files via upload

--- HB_classLib.py
+++ HB_classLib.py
@@ -2,25 +2,83 @@ import pygame as pg
 from random import shuffle
 from enum import Enum
 
-def Lvls(Enum):
+class LvlMap:
+    def __init__(self):
+        self.textures = {
+            'RIVER' : pg.image.load("images\\river.png"),
+            'MNTIN' : pg.image.load("images\\mountain.png"),
+            'PLAIN' : pg.image.load("images\\plain.png"),
+            'FOREST': pg.image.load("images\\forest.png")
+        }
+        self.tilemap = []
+        self.tilesize = 32
+
+    def gen(self, pl):
+        self.tilemap.clear()
+        match pl.progress:
+            case 0:
+                self.tilemap.append([self.textures['RIVER']*8])
+                for i in range(4):
+                    self.tilemap.append([self.textures['PLAIN']*8])
+                self.tilemap.append([self.textures['RIVER']*8])
+        
+
+    def draw(window):
+        for row in range(len(self.tilemap) ):
+            for column in range(len(self.tilemap[row]) ):
+                window.blit(self.textures[self.tilemap[row][column]],
+                    (column*self.tilesize, row*self.tilesize))
+
+class Map:
+    def __init__(self):
+        self.should_draw = True
+        self.objs_to_draw = [Image("images\\bg_MAP", (0, 0))]
+    def draw(self, scr):
+        if self.should_draw:
+            for elem in self.objs_to_draw:
+                elem.draw(scr)
+    def fill_with(self, objs):
+        self.objs_to_draw += objs
+        
+        
+
+class Button:
+    def __init__(self, pos, size, color, text, image, **kwargs):
+        self.pos = pos
+        self.size = size
+        self.color = color
+        self.text = text
+        self.image = image
+
+
+class Lvls(Enum):
     """Class for more cozy operating with levels"""
     TUTORIAL = 0
     FIRST = 1
     SECOND = 2
     LAST = 3
 
-class Text(pygame.sprite.Sprite):
+class Text(pg.sprite.Sprite):
     """Text class"""
     def __init__(self, text, size, color, font=None, **kwargs):
         super(Text, self).__init__()
         self.color = color
-        self.font = pygame.font.Font(font, size)
+        self.font = pg.font.Font(font, size)
         self.kwargs = kwargs
         self.set(text)
 
     def set(self, text):
         self.image = self.font.render(str(text), 1, self.color)
         self.rect = self.image.get_rect(**self.kwargs)
+
+class Image(pg.sprite.Sprite):
+    """image class"""
+    def __init__(self, img_fn, pos):
+        self.image = pg.image.load(img_fn)
+        self.pos = pos
+    def draw(self, scr):
+        scr.blit(self.image.get_rect(), self.pos)
+        
 
 class BaseCard(pg.sprite.Sprite):
     """Parent class of all cards"""
@@ -33,6 +91,7 @@ class BaseCard(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.pos = pos
         self.not_used = True
+
 
     def draw(self, scr):
         """Draw on screen cards"""
@@ -52,16 +111,19 @@ class BaseCard(pg.sprite.Sprite):
         
         
 
-class RangerCard:
+class RangerCard(BaseCard):
     """Archers and more..."""
+    #TODO shoot <function> and class charasterics
     pass
 
-class MeleeCard:
+class MeleeCard(BaseCard):
     """Melee units & spearmen"""
+    #TODO damage <function>
     pass
 
-class CavalryCard:
+class CavalryCard(BaseCard):
     """Cavalry and elephants"""
+    #TODO damage <function> as a charge <function>
     pass
 
 class Player:
@@ -69,6 +131,10 @@ class Player:
     def __init__(self):
         self.hand = []
         self.progress = Lvls.TUTORIAL
+
+    def upStats(self):
+        if self.progress.value < 3:
+            self.progress = Lvls(self.progress.value + 1)
 
 class Hand:
     """Player's hand """
--- engine.py
+++ engine.py
@@ -4,9 +4,14 @@ import HB_classLib as cllib
 
 class Engine:
     """Engine which will calculate gameClasses's interactions"""
-    def __init__(self):
+    def __init__(self, screen):
+
+        self.screen = screen
+        
         self.pl = cllib.Player()
         self.map = cllib.Map()
+        self.currlvlmap = cllib.LvlMap()
+        #TODO: replace cllib.ButtonTutorial with cllib.Button(...)
         self.lvls = [cllib.ButtonTutorial, cllib.ButtonLvl1,
                      cllib.ButtonLvl2, cllib.ButtonLvl3]
         self.start_cards = [cllib.MeleeCard("HB_army", health=5,attack=3),
@@ -15,5 +20,12 @@ class Engine:
     def start(self):
         self.pl.hand.fill(self.start_cards)
         self.map.fill_with(self.lvls)
+        self.map.draw(self.screen)
+
+    def turn_level(self):
+        self.map.should_draw = False
+        self.currlvlmap.gen(self.pl)
+        self.currlvlmap.draw(self.screen)
+        
         
     
